9badb01f0659761d9508c5ae0c2758c7fb264150
Taha <tahayasinbalaban41@gmail.com>
2026-03-29 18:53:16 +0300

    Fix typescript error in LandingPage

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -19,7 +19,7 @@ export const LandingPage = () => {
 
   const itemVariants = {
     hidden: { opacity: 0, y: 15 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } }
+    visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" as const } }
   };
 
   return (
